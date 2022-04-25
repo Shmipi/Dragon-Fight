40e56fc76ec6a8d56f39a209d3e5b6258093876d
Beheleit <robhja92@hotmail.com>
2022-04-25 18:41:01 +0200

    Sound Controller

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+
+    //This class makes it possible to referance Assets through code
+    //Just attachthe code to a GameObject and drag said Asset into the open field.
+
+    private static GameAssets _i;
+
+    public static GameAssets i
+    {
+        get {
+            if (_i == null) 
+                _i = (Instantiate(Resources.Load("GameAssets")) as GameAssets).GetComponent<GameAssets>();
+            return _i; 
+        }
+    }
+
+    //Insert what type of Asset you whant to referance
+    // example: public Sprite nameOfSprite;
+
+    public SoundAudioClip[] soundAudioClipArray;
+
+    [System.Serializable]   
+    public class SoundAudioClip //Stores Audio referances.
+    {
+        public SoundFXController.Sound sound;
+        public AudioClip audioClip;
+
+    }
+
+}
---
+++ Assets/Scripts/SoundFXController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SoundFXController
+{
+
+    public enum Sound //Holds the referance to the audioclips through GameAssets
+    {
+        PlayerMove,
+        PlayerAttack,
+    }
+
+    private static Dictionary<Sound, float> soundTimerDictionary; //Timmer for making sound play under certain time
+
+    private static void Initialize() // used to initialize Dictionary. Should be called in "Awake" when game starts
+    {
+        soundTimerDictionary = new Dictionary<Sound, float>();
+        soundTimerDictionary[Sound.PlayerMove] = 0f;
+    }
+
+    public static void PlaySound(Sound sound) //choses wich sound should be played
+    {
+        if (CanPlaySound(sound))
+        {
+            GameObject soundGameObject = new GameObject("Sound");
+            AudioSource audioSource = soundGameObject.AddComponent<AudioSource>(); //this komponent plays the Audio
+            audioSource.PlayOneShot(GetAudioClip(sound));  // funktion that choses how the sound should be played
+        }
+    }
+
+
+    private static bool CanPlaySound(Sound sound) //checks if sound chould be played normal or with delay
+    {
+        switch (sound)
+        {
+            default:
+                return true; // most sounds will be played normaly, thus only specifik sounds will be fals
+            case Sound.PlayerMove: // can use for several sounds such as PlayerAttack etc..
+                if (soundTimerDictionary.ContainsKey(sound))
+                {
+                    float lastTimePlayed = soundTimerDictionary[sound];
+                    float playerMoveTimerMax = .05f; // time that is delayed
+                    if(lastTimePlayed + playerMoveTimerMax < Time.time)
+                    {
+                        soundTimerDictionary[sound] = Time.time;
+                        return true;
+                    }
+                    else { return false;}
+                }
+                else { return true;}
+        }
+    }
+
+
+    //finds the right audioclip to be played by the PlaySound class
+    private static AudioClip GetAudioClip(Sound sound)
+    {
+        foreach (GameAssets.SoundAudioClip soundAudioClip in GameAssets.i.soundAudioClipArray)  //cycles through the array
+        {
+            if (soundAudioClip.sound == sound)
+            {
+                return soundAudioClip.audioClip;  
+            } 
+        }
+        Debug.LogError("Sound " + sound + " not found");
+        return null;
+    }  //To use insert: SoundFXController.PlaySound(SoundFXController.Sound.NameOfSound);
+
+
+}
