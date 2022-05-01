830406df9ddf337f843ceac0f6fffefb8d36b981
Qvistens <89694190+Qvistens@users.noreply.github.com>
2022-05-01 12:01:41 +0200

    MainMenu
    Början av en huvudmeny

---
+++ Assets/Scripts/MainMenu/MainMenu_PlayLevel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_PlayLevel : MonoBehaviour
+{
+    public void PlayLevelOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void PlayLevelTwo()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void PlayLevelThree()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void PlayLevelFour()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
