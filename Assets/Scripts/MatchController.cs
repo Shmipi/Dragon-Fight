892ffdbde144352fd0d13f00850533f4392956cf
YumeNeDai <frecuencia_sec@hotmail.com>
2022-05-01 21:42:16 +0200

    Revert "Fighting updates"
    This reverts commit 93ac3256a21ce0cc6dcc674e261a05c5648cb8d2.

--- Assets/Scripts/MatchController.cs
+++ Assets/Scripts/MatchController.cs
@@ -1,23 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MatchController : MonoBehaviour
 {
-
-    [SerializeField] private GameObject winPanel;
-    [SerializeField] private Text winText;
-
-    [SerializeField] private HealthTest player1Health;
-    [SerializeField] private HealthTest player2Health;
-
-    [SerializeField] private Button p1Attack;
-    [SerializeField] private Button p1Move;
-    [SerializeField] private Button p2Attack;
-    [SerializeField] private Button p2Move;
-
-    [SerializeField] private GameObject restartButton;
 
     [SerializeField] private GameObject player1;
     [SerializeField] private GameObject player2;
@@ -25,20 +11,7 @@ public class MatchController : MonoBehaviour
 
     private void Awake()
     {
-        MatchStart();
-    }
-
-    public void MatchStart()
-    {
         StartCoroutine(StartGame());
-        winPanel.SetActive(false);
-        restartButton.SetActive(false);
-        player1Health.ResetHealth();
-        player2Health.ResetHealth();
-        p1Attack.interactable = true;
-        p1Move.interactable = true;
-        p2Attack.interactable = true;
-        p2Move.interactable = true;
     }
 
     private IEnumerator StartGame()
@@ -54,31 +27,6 @@ public class MatchController : MonoBehaviour
         player2.gameObject.GetComponent<PlayerBehaviour>().StartSpawn();
         print("Go!");
        
-    }
-
-    private void Update()
-    {
-        if (player1Health.currentHealth < 1)
-        {
-
-            winPanel.SetActive(true);
-            winText.text = "GREEN DRAGON WINS!";
-            p1Attack.interactable = false;
-            p1Move.interactable = false;
-            p2Attack.interactable = false;
-            p2Move.interactable = false;
-            restartButton.SetActive(true);
-        }
-        else if (player2Health.currentHealth < 1)
-        {
-            winPanel.SetActive(true);
-            winText.text = "RED DRAGON WINS!";
-            p1Attack.interactable = false;
-            p1Move.interactable = false;
-            p2Attack.interactable = false;
-            p2Move.interactable = false;
-            restartButton.SetActive(true);
-        }
     }
 
 }
